bb46caef1d48d93aa3feb424b1d017bee7d67836
david salazar <davidssm15gmail.com>
2024-02-06 19:48:23 -0500

    se realizo la operacion total y se imprimio la factura

--- productos.py
+++ productos.py
@@ -8,3 +8,11 @@ iva = 0.19
 total_con_descuento = subtotal - (subtotal * descuento)
 total_con_iva = str (total_con_descuento + (total_con_descuento * iva))
 
+print("\n=====FACTURA=====")
+print("Nombre: " , nombre)
+print("Cantidad" , cantidad)
+print("Precio: " , precio)
+print("___________________")
+print("Subtotal: " , subtotal)
+print("El total a pagar es: " + total_con_iva)
+print("====================")
